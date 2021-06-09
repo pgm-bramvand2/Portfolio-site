cd4b46360c9fa4eeef72a0bc275c347ab5328a6c
pgm-bramvand2 <pgm-bramvand2@users.noreply.github.com>
2021-06-09 23:53:01 +0000

    deploy: a8756796f52782505fd00df9fef4d21adcfb275a

--- static/js/main.js
+++ static/js/main.js
@@ -3,24 +3,21 @@ import { MAPBOX_TOKEN, MAP_OPTIONS, OFFICE_COORDS } from './constants.js'
 (() => {
   const app = {
     initialize () {
-      this.map = null;
-      this.marker = null;
       this.cacheElements();
       this.registerEventListeners();
-      this.typeWriter();
-      this.navBgChange();
-      this.loadOnView();
-      this.createMapAndMarker();
-    },
-    cacheElements () {
-      this.btnToTopElement = document.querySelector('.btn-to-top');
-      this.hamburger = document.querySelector('.hamburger');
-      this.navItems = document.querySelector('.nav__list');
-      this.subjectSelect = document.querySelector('#user__message-subject');
-      this.fileField = document.querySelector('#file-field');
+      this.handleScroll();
+      this.smoothAnchor();
+
     },
 
+    cacheElements () {
+      this.btnToTopElement = document.querySelector('#to-top-button');
+      this.anchors = document.querySelectorAll('a[href^="#"]');
+    },
+    
     registerEventListeners () {
+      document.addEventListener("scroll", this.handleScroll);
+      
       if (this.btnToTopElement !== null) {
         this.btnToTopElement.addEventListener('click', (ev) => {
           window.scrollTo({
@@ -30,97 +27,32 @@ import { MAPBOX_TOKEN, MAP_OPTIONS, OFFICE_COORDS } from './constants.js'
           });
         });
       }
-
-      if (this.subjectSelect !== null) {
-        this.subjectSelect.addEventListener('change', (e) => {
-          if(e.target.value == 'job-application') {
-            this.fileField.classList.remove('hidden');
-          } else {
-            this.fileField.classList.add('hidden');
-          };
-        });
+    },
+    
+    handleScroll () {
+      const rootElement = document.documentElement;
+      const button = document.querySelector('#to-top-button');
+      let scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+      if((rootElement.scrollTop / scrollTotal) > 0.20) {
+        button.classList.add("shown");
+      } else {
+        button.classList.remove("shown");
       };
-
-      this.hamburger.addEventListener('click', () => {
-        if (!this.navItems.classList.contains('show')) {
-          this.navItems.classList.add('show');
-        } else {
-          this.navItems.classList.remove('show');
-        }
-      });
     },
 
-    typeWriter() {
-      console.log(document.title)
-      const title = document.title.slice(-63, -55);
-      const homeTitle = 'The Fellowship'
-      console.log(title)
-      let letter = '';
-      let index = 0;
-
-      (function type() {
-        if (title === 'Home') {
-          letter = homeTitle.slice(0, ++index);
-        } else {
-          letter = title.slice(0, ++index);
-        }
-        const typer = document.querySelector('.typer');
-        if (typer !== null) {
-          typer.innerHTML = letter;
-        }
-
-        setTimeout(type, 200);
-      }())
-    },
-
-    navBgChange () {
-      window.onscroll = function() {
-        const navFixed = document.querySelector('.text-container');
-        if (!navFixed.classList.contains('bgcolor')) {
-          navFixed.classList.add('bgcolor');
-        };
-      
-        if (window.scrollY == 0 && navFixed.classList.contains('bgcolor')) {
-          navFixed.classList.remove('bgcolor');
-        };
-      }
-    },
-
-    loadOnView () {
-      let scroll = window.requestAnimationFrame || function(callback) {
-          window.setTimeout(callback, 1000/60);
-      }
-      
-      let elements = document.querySelectorAll('.show-on-scroll');
-      function loop () {
-          elements.forEach(function(element) {
-              if (isElementInViewpoint(element)) {
-                  element.classList.add('is-visible');
-              } else {
-                  element.classList.remove('is-visible');
-              }
+    smoothAnchor () {
+      this.anchors.forEach( anchor => {
+        anchor.addEventListener('click', (e) => {
+          e.preventDefault();
+          document.querySelector(e.target.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
           });
-      
-          scroll(loop);
-      }
-      
-      loop();
-      
-      function isElementInViewpoint(el) {
-          let rect = el.getBoundingClientRect();
-          return (
-              (rect.top <= 0 && rect.bottom >= 0 || (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) && rect.top <= (window.innerHeight || document.documentElement.clientHeight)) || (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)))
-          );
-      }
-    },
-
-    createMapAndMarker() {
-      mapboxgl.accessToken = MAPBOX_TOKEN;
-      this.map = new mapboxgl.Map(MAP_OPTIONS);
-      this.marker = new mapboxgl.Marker()
-        .setLngLat(OFFICE_COORDS)
-        .addTo(this.map);
+        });
+      });
     }
+
+    
+ 
   };
   app.initialize();
 })();
